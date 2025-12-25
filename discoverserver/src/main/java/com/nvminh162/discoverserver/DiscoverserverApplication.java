b7584129aba0ddb434d405c29dc0e97ba33e147b
nvminh162 <nvminh162@gmail.com>
2025-12-26 00:40:23 +0700

    setup discoverserver

--- discoverserver/src/main/java/com/nvminh162/discoverserver/DiscoverserverApplication.java
+++ discoverserver/src/main/java/com/nvminh162/discoverserver/DiscoverserverApplication.java
@@ -2,8 +2,12 @@ package com.nvminh162.discoverserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// https://spring.io/guides/gs/service-registration-and-discovery
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoverserverApplication {
 
 	public static void main(String[] args) {
